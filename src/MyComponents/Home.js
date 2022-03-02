0992b84383c4e2ce9077a65ad52771a2f929ed31
Mr.Pritish <64859883+MrPritish@users.noreply.github.com>
2022-03-02 11:30:04 +0530

    Add files via upload

---
+++ src/MyComponents/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import bcimg from './logo.png';
+import mypic from './profpic.jpg'
+import { Link } from 'react-router-dom';
+
+export default function home() {
+  return (
+    <>
+
+      <div className="loadbar"></div>
+
+      <div className='lg:flex lg:flex-row-reverse '>
+
+        <div className=' items-center justify-center flex '>
+
+          <img className='w-auto px-10 sm:px-28 md:px-36 lg:px-10' src={bcimg} alt="" />
+
+        </div>
+
+        <div className='text-white rounded-tl-[20%] rounded-br-[20%] m-2 lg:m-5 lg:mt-6 p-8 lg:p-14 border-blue-400 text-center lg:w-4/5 items-center justify-center'>
+
+          <img className='rounded-2xl border-2 sm:w-80 border-white m-auto md:w-96 w-72 lg:w-80' src={mypic} alt="img" />
+
+          <h1 className='mt-10 md:text-5xl text-4xl font-bold'>Hello,I'm <br></br>Pritish Chepure</h1>
+
+          <p className='text-xl mt-4 md:text-2xl sm:text-2xl font-medium text-red-500'>Web Developer & Python Developer</p>
+
+          <button name='button' className=' px-6 text-lg lg:text-2xl py-2 mt-5 border-2 text-white shadow-md shadow-black lg:active:scale-90  hover:border-blue-500 rounded-full bg-cyan-700 hover:bg-cyan-900 font-bold lg:hover:scale-110 '><Link to="/about">ABOUT ME</Link></button>
+
+        </div>
+
+      </div>
+
+    </>
+  )
+}
---
+++ src/MyComponents/Skills.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <>
+
+      <div className="loadbar"></div>
+
+      <div className='text-white rounded-tl-[20%] rounded-br-[20%] m-2 lg:m-5 lg:mt-6 p-8 lg:p-14 border-[2px] border-blue-400 items-center justify-center flex-col space-y-6 mt-14 flex'><div>
+
+        <h2 className='font-bold text-4xl text-center lg:text-5xl '>Skills</h2>
+
+      </div>
+
+        <div>
+
+          <h3 className='text-xl lg:text-3xl font-medium'>Programming Languages </h3>
+
+        </div>
+
+        <div className='font-medium flex columns-4 items-center justify-center lg:text-2xl text-lg text-[lightblue] flex-row flex-wrap space-x-9 space-y-2'>
+
+          <div className='flex'><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" className='w-4 lg:w-7' alt="" />Python</div>
+
+          <div className='flex'><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" className='w-4 lg:w-7' alt="" />HTML</div>
+
+          <div className='flex'><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" className='w-4 lg:w-7' alt="" />CSS</div>
+
+          <div className='flex'> <img className='w-5' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" className='w-3 lg:w-6' alt="" />JavaScript</div>
+
+        </div>
+        <div>
+
+          <h3 className='text-xl font-medium lg:text-3xl'>Technologies/Frameworks</h3>
+
+        </div>
+
+        <div className='font-medium flex items-center justify-center lg:text-2xl text-lg text-[lightblue] flex-row flex-wrap space-x-9 space-y-2'>
+
+          <div className="flex"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" className='w-4 lg:w-7' alt="" />Bootstrap</div>
+
+          <div className="flex"><img src="https://raw.githubusercontent.com/detain/svg-logos/780f25886640cef088af994181646db2f6b1a3f8/svg/selenium-logo.svg" className='w-4 lg:w-7' alt="" />Selenium</div>
+
+          <div className="flex"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" className='w-4 lg:w-7' alt="" />React.js</div>
+
+          <div className="flex"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/django/django-original.svg" className='w-4 lg:w-7' alt="" />Django</div>
+
+          <div className="flex"><img src="https://camo.githubusercontent.com/5734d0669fe22ce04a1cb989a156cd32c379875f6bca56d5210c9432824856d9/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667" className='w-4 lg:w-7' alt="" />Tailwind</div>
+
+          <div className="flex"><img src="https://camo.githubusercontent.com/cb2324a4c0e1910089f481d56e1f887d6e96114101987dfbb6ef6f9df1e0bf08/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f706f636f6f5f666c61736b2f706f636f6f5f666c61736b2d69636f6e2e737667" className='w-4' alt="" />Flask</div>
+
+          <div className="flex"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" className='w-4 lg:w-7' alt="" />Node.js</div>
+
+          <div className="flex"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" className='w-4 lg:w-7' alt="" />Adobe Photoshop</div>
+
+          <div className="flex"><img src="https://camo.githubusercontent.com/9e245893108b5ca27e7ac3d4a802d513f657b32aa7b5765bd92df7fb55d0ed54/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f61646f62655f696c6c7573747261746f722f61646f62655f696c6c7573747261746f722d69636f6e2e737667" className='h-4' alt="" />Adobe Illustrator</div>
+
+        </div>
+
+        <div>
+
+          <h3 className='text-xl font-medium lg:text-3xl'>Databases</h3>
+
+        </div>
+
+        <div className='font-medium flex items-center justify-center lg:text-2xl text-lg text-[lightblue] flex-row flex-wrap space-x-9 space-y-2'>
+
+          <div className="flex"><img src="https://camo.githubusercontent.com/1b8a779f280e099e2d67ab949dad604e25ce0d321e66474c04430201790b3874/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f73716c6974652f73716c6974652d69636f6e2e737667" className='w-4 lg:w-7' alt="" />sqlite</div>
+
+          <div className="flex"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" className='w-4 lg:w-7' alt="" />MySQL</div>
+
+        </div>
+
+      </div>
+
+    </>
+  )
+}
